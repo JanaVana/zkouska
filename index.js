2c1a01adfc38907b650e1ab9dcdd4aecf7adab36
JanaVana <vanova.yana@gmail.com>
2022-02-23 22:43:30 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-//Nadefinujme si proměnné a jejich hodnoty potřebné k uložení následujících informací:
- //Jméno muže a ženy ​
- //Rok a měsíc narození muže i ženy​
-//Spočteme rozdíl věku muže a ženy v měsících a vypíšeme ve formátu​ "[muz] a [zena] jsou od sebe věkově vzdáleni [XY] měsíců​"
-//Pro zjednodušení úkolu počítejme, že je muž vždy starší! (ať nemusíme řešit absolutní hodnotu z Math.abs())
+let jmeno = prompt ('jak se jmenujes');
 
-let jmenoMuze = 'Pavel';
-let jmenoZeny = 'Jitka';
-
-let rokNarozeniZena = 1990
-let rokNarozeniMuz = 1985
-
-let mesicNarozeniZena = 2
-let mesicNarozeniMuz = 5
-
-let vysledek = (rokNarozeniZena - rokNarozeniMuz) * 12 + (mesicNarozeniZena-mesicNarozeniMuz);
-
-console.log(jmenoMuze + ' a ' + jmenoZeny + ' jsou od sebe vekove vzdaleni ' + vysledek + ' mesicu');
+console.log('Jmenuji se ' + jmeno);
